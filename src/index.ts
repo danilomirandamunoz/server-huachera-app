63492395351134956cb538ef7b3fc8f89f708206
danilo <danilo.miranda.munoz@gmail.com>
2018-08-17 15:32:59 -0300

    make it better

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Server from "./server/server";
 import router from './routes/router';
 import MySQL from './mysql/mysql';
 
-const server = Server.init(3000);
+const server = Server.init(8080);
 server.app.use("/api",router);
 
 const mysql = new MySQL();
